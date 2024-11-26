872cb6398a3d6ea790fef9a5e4e178a9d2979349
cipher <fynhacker@gmail.com>
2024-11-26 12:00:30 +0530

    adding additional attribute to the product schema

--- models/product.js
+++ models/product.js
@@ -4,6 +4,7 @@ const productSchema = mongoose.Schema({
     name : String,
     price : Number,
     description : String,
+    lastPrice : Number,
 })
 
 const Product = mongoose.model("products",productSchema)
