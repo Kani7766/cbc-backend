811216c293b7c9f30ee04b063cd6bda690534c68
cipher <fynhacker@gmail.com>
2024-12-07 20:01:15 +0530

    fix

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,4 +1,5 @@
 import e from "express";
+import orderControllers from "../controllers/orderControllers.js";
 import { createOrder, getOrders } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
